ed1d442eab3a8d081257a576d2f05f0c8f76a23c
P saikumar <saikumar.patnaikuni@kelltontech.com>
2017-12-08 14:45:07 +0530

    new htmls

---
+++ header.php
@@ -0,0 +1,309 @@
+<header class="main-header"> 
+    <!-- Logo --> 
+    
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <div class="logo-sec"> Project X</div>
+      
+      <!-- Sidebar toggle button-->
+      <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#">Alarms</a></li>
+          <li><a href="#">Events</a></li>
+          <li><a href="#">Events</a></li>
+          <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Operations <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Action1</a></li>
+              <li><a href="#">Action2</a></li>
+              <li><a href="#">Action3</a></li>
+              <li><a href="#">Action4</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Contract Mangement</a></li>
+        </ul>
+      </div>
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li class="dropdown notifications-menu"> <a class="dropdown-toggle" data-toggle="modal" data-target="#product_view1"> <i class="fa fa-bell-o"></i> <span class="label label-warning">10</span>
+            <div class="alarm-notification-sec">
+              <div class="modal fade product_view" id="product_view1">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-body">
+                          <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+                            <div class="row">
+                              <div class="col-lg-12">
+                                <h3><span>A</span>larm</h3>
+                              </div>
+                              <div class="col-lg-12">
+                                <div class="row">
+                                  <div class="col-lg-4 marg-t-15">
+                                    <p>Alarms Total:90</p>
+                                    <p>Not Acknowledged:20</p>
+                                  </div>
+                                  <div class="col-lg-2 center-block">
+                                    <div class="dashedborder-vlow">15</div>
+                                    <p>Very Low</p>
+                                  </div>
+                                  <div class="col-lg-2 center-block">
+                                    <div class="dashedborder-vhigh">15</div>
+                                    <p>Very High</p>
+                                  </div>
+                                  <div class="col-lg-2 center-block">
+                                    <div class="dashedborder-low">15</div>
+                                    <p>Low</p>
+                                  </div>
+                                  <div class="col-lg-2 center-block">
+                                    <div class="dashedborder-high">15</div>
+                                    <p>High</p>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                              <div>
+                                <div class="col-lg-6">
+                                  <h3><span>A</span>larm Notifications</h3>
+                                </div>
+                                <div class="col-sm-6 marg-t-15 text-right alarm-notification-search"><div id="example1_filter" class="dataTables_filter"><label>Filterd by: <input type="search" class="form-control input-sm" placeholder="" aria-controls="example1"></label></div></div>
+                              </div>
+                              <div class="col-sm-12">
+                                <table id="example1" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="example1_info">
+                                  <thead>
+                                    <tr role="row">
+                                      <th>Action</th>
+                                      <th>Client</th>
+                                      <th>Country</th>
+                                      <th>Filed</th>
+                                      <th>Pad</th>
+                                      <th>Well</th>
+                                      <th>Alarm Time</th>
+                                      <th>Status</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    <tr role="row" class="odd">
+                                      <td><div class="checkbox">
+                                          <label>
+                                            <input type="checkbox">
+                                          </label>
+                                        </div></td>
+                                      <td >Lorem</td>
+                                      <td>US</td>
+                                      <td>14</td>
+                                      <td>18</td>
+                                      <td>D-376</td>
+                                      <td>3:00PM</td>
+                                      <td><i class="fa fa-circle text-red"></i> WO Opt</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                      <td><div class="checkbox">
+                                          <label>
+                                            <input type="checkbox">
+                                          </label>
+                                        </div></td>
+                                      <td >Lorem</td>
+                                      <td>US</td>
+                                      <td>14</td>
+                                      <td>18</td>
+                                      <td>D-376</td>
+                                      <td>3:00PM</td>
+                                      <td><i class="fa fa-circle text-red"></i> WO Opt</td>
+                                    </tr>
+                                    <tr role="row" class="even">
+                                      <td><div class="checkbox">
+                                          <label>
+                                            <input type="checkbox">
+                                          </label>
+                                        </div></td>
+                                      <td >Lorem</td>
+                                      <td>US</td>
+                                      <td>14</td>
+                                      <td>18</td>
+                                      <td>D-376</td>
+                                      <td>3:00PM</td>
+                                      <td><i class="fa fa-circle text-yellow"></i> WO Opt</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                      <td><div class="checkbox">
+                                          <label>
+                                            <input type="checkbox">
+                                          </label>
+                                        </div></td>
+                                      <td >Lorem</td>
+                                      <td>US</td>
+                                      <td>14</td>
+                                      <td>18</td>
+                                      <td>D-376</td>
+                                      <td>3:00PM</td>
+                                      <td><i class="fa fa-circle text-yellow"></i> WO Opt</td>
+                                    </tr>
+                                    <tr role="row" class="even">
+                                      <td><div class="checkbox">
+                                          <label>
+                                            <input type="checkbox">
+                                          </label>
+                                        </div></td>
+                                      <td >Lorem</td>
+                                      <td>US</td>
+                                      <td>14</td>
+                                      <td>18</td>
+                                      <td>D-376</td>
+                                      <td>3:00PM</td>
+                                      <td><i class="fa fa-circle text-orange"></i> WO Opt</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                      <td><div class="checkbox">
+                                          <label>
+                                            <input type="checkbox">
+                                          </label>
+                                        </div></td>
+                                      <td >Lorem</td>
+                                      <td>US</td>
+                                      <td>14</td>
+                                      <td>18</td>
+                                      <td>D-376</td>
+                                      <td>3:00PM</td>
+                                      <td><i class="fa fa-circle text-green"></i> WO Opt</td>
+                                    </tr>
+                                    <tr role="row" class="even">
+                                      <td><div class="checkbox">
+                                          <label>
+                                            <input type="checkbox">
+                                          </label>
+                                        </div></td>
+                                      <td >Lorem</td>
+                                      <td>US</td>
+                                      <td>14</td>
+                                      <td>18</td>
+                                      <td>D-376</td>
+                                      <td>3:00PM</td>
+                                      <td><i class="fa fa-circle text-brown"></i> WO Opt</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                      <td><div class="checkbox">
+                                          <label>
+                                            <input type="checkbox">
+                                          </label>
+                                        </div></td>
+                                      <td >Lorem</td>
+                                      <td>US</td>
+                                      <td>14</td>
+                                      <td>18</td>
+                                      <td>D-376</td>
+                                      <td>3:00PM</td>
+                                      <td><i class="fa fa-circle text-blue"></i> WO Opt</td>
+                                    </tr>
+                                    <tr role="row" class="even">
+                                      <td><div class="checkbox">
+                                          <label>
+                                            <input type="checkbox">
+                                          </label>
+                                        </div></td>
+                                      <td >Lorem</td>
+                                      <td>US</td>
+                                      <td>14</td>
+                                      <td>18</td>
+                                      <td>D-376</td>
+                                      <td>3:00PM</td>
+                                      <td><i class="fa fa-circle text-red"></i> WO Opt</td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </div>
+                            </div>
+                                  <hr>
+                               <div class="row">
+                              <div>
+                                <div class="col-lg-6">
+                                  <h3><span>A</span>ssets Status</h3>
+                                </div>
+                                
+                              </div>
+                              
+                              <div class="row"><div class="col-sm-12 marg-t-15">
+                                <div class="col-lg-3 text-red"><i class="fa fa-circle"></i> Failed= 16</div>
+                                <div class="col-lg-3 text-orange"><i class="fa fa-circle"></i> WO Opt=10</div>
+                                <div class="col-lg-3 text-yellow"><i class="fa fa-circle"></i> Stopped= 17</div>
+                                <div class="col-lg-3 text-brown"><i class="fa fa-circle"></i> WO Installation=4</div>
+                                <div class="col-lg-3 text-green marg-t-15"><i class="fa fa-circle"></i> Running=4</div>
+                                <div class="col-lg-3 text-blue marg-t-15"><i class="fa fa-circle"></i> WO Pull=19</div>
+                              </div></div>
+                              
+                              <div class="col-lg-12 marg-t-15">
+
+                                <button type="button" class="btn btn-primary">Export Data</button>
+                                <button type="button" class="btn btn-primary">Email Data</button>
+
+                              
+                            </div>
+                            </div>   
+                            
+                          </div>
+                        </div>
+                        <!-- /.box-body --> 
+                        
+                      </div>
+
+                </div>
+              </div>
+            </div>
+            </a> </li>
+          <!-- Tasks: style can be found in dropdown.less -->
+<!--          <li class="dropdown tasks-menu"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-clock-o"></i> <span class="label label-danger">9</span> </a>
+            <ul class="dropdown-menu">
+              <li class="header">You have 9 tasks</li>
+              <li> 
+                 inner menu: contains the actual data 
+                <ul class="menu">
+                  <li> Task item  
+                    <a href="#">
+                    <h3> Design some buttons <small class="pull-right">20%</small> </h3>
+                    <div class="progress xs">
+                      <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"> <span class="sr-only">20% Complete</span> </div>
+                    </div>
+                    </a> </li>
+                   end task item 
+                </ul>
+              </li>
+              <li class="footer"> <a href="#">View all tasks</a> </li>
+            </ul>
+          </li>-->
+          <!-- User Account: style can be found in dropdown.less -->
+          <li class="dropdown user user-menu"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image"> <span class="hidden-xs"> Wellcome, User <i class="fa fa-angle-down"></i></span> <br>
+            <small>Last Login 15</small> </a>
+            <ul class="dropdown-menu">
+              <!-- User image -->
+              <li class="user-header"> <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <p> Name <small>Member since Nov. 2017</small> </p>
+              </li>
+              <!-- Menu Body -->
+              <li class="user-body">
+<!--                <div class="row">
+                  <div class="col-xs-4 text-center"> <a href="#">Followers</a> </div>
+                  <div class="col-xs-4 text-center"> <a href="#">Sales</a> </div>
+                  <div class="col-xs-4 text-center"> <a href="#">Friends</a> </div>
+                </div>-->
+                <!-- /.row --> 
+              </li>
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-left"> <a href="#" class="btn btn-default btn-flat">Profile</a> </div>
+                <div class="pull-right"> <a href="javascript:void(0);" id = "logout" class="btn btn-default btn-flat">Sign out</a> </div>
+              </li>
+            </ul>
+          </li>
+          <!-- Control Sidebar Toggle Button -->
+          
+        </ul>
+      </div>
+    </nav>
+  </header>
+<?php include 'root_below.php'; ?>
+<script>
+    $("#logout").click(function(){
+	window.location.href = "logout.php";
+    });
+</script>
